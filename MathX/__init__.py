40082109610b43d24a23553e11272c0caa34380e
DanikArduino <144534634+DanikArduino@users.noreply.github.com>
2025-06-26 09:30:23 +0300

    Add files via upload

---
+++ MathX/__init__.py
@@ -0,0 +1,16 @@
+from .core import (
+    isPrime, isPalindrome, isFibonacci,
+    fibonacci, fibonacciSubsequence, fibonacciSubsequenceQuantity,
+    prime, primeSubsequence, primeSubsequenceQuantity,
+    factorize, gcdMultiple, lcmMultiple,
+    arithmeticMean, geometricMean
+)
+
+__version__ = "1.0.0"
+__all__ = [
+    'isPrime', 'isPalindrome', 'isFibonacci',
+    'fibonacci', 'fibonacciSubsequence', 'fibonacciSubsequenceQuantity',
+    'prime', 'primeSubsequence', 'primeSubsequenceQuantity',
+    'factorize', 'gcdMultiple', 'lcmMultiple',
+    'arithmeticMean', 'geometricMean'
+]
