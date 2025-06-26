006a7786a81f88cfd0b7c1a334625d9519312df1
DanikArduino <144534634+DanikArduino@users.noreply.github.com>
2025-06-26 09:28:39 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name='mathx',
+    version='1.0.0',
+    author='daniil',
+    author_email='danik20110723@gmail.com',
+    url='',
+    description='Extended math library for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    packages=find_packages(),
+    pyton_requires='>=3.7',
+    install_requires=[],
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
